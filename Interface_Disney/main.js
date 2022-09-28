2860c7f74c220e31f5d1f66fd559ef10acf0e37a
Alex Diaz <geovanialex5@hotmail.com>
2022-09-28 17:24:21 -0500

    Subi mi practica

---
+++ Interface_Disney/main.js
@@ -0,0 +1,33 @@
+const muyPronto = document.getElementById('Pronto');
+const Tendencia = document.getElementById('Tendencia');
+const masStar = document.getElementById('MasStar');
+
+function pronto(){
+    document.getElementById('muyPronto').classList.remove('Ocultar');
+    document.getElementById('masStar').classList.add('Ocultar');
+    document.getElementById('tendecia').classList.add('Ocultar');
+    muyPronto.classList.add('select');
+    Tendencia.classList.remove('select');
+    masStar.classList.remove('select');
+}
+
+function tendecia(){
+    document.getElementById('muyPronto').classList.add('Ocultar');
+    document.getElementById('masStar').classList.add('Ocultar');
+    document.getElementById('tendecia').classList.remove('Ocultar');
+    Tendencia.classList.add('select');
+    muyPronto.classList.remove('select');
+    masStar.classList.remove('select');
+}
+function maasStar(){
+    document.getElementById('muyPronto').classList.add('Ocultar');
+    document.getElementById('tendecia').classList.add('Ocultar');
+    document.getElementById('masStar').classList.remove('Ocultar');
+    masStar.classList.add('select');
+    Tendencia.classList.remove('select');
+    muyPronto.classList.remove('select');
+}
+
+masStar.addEventListener('click',maasStar)
+Tendencia.addEventListener('click', tendecia);
+muyPronto.addEventListener('click',pronto);
